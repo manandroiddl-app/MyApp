6fc9260e17609fb67c34e3c461da18ace8f95183
manandroiddl-app <man.android.dl@gmail.com>
2026-08-10 23:32:35 +0800

    Create NetworkModule.kt

---
+++ app/src/main/java/com/example/lifeapp/di/NetworkModule.kt
@@ -0,0 +1,32 @@
+package com.example.lifeapp.di
+
+import com.example.lifeapp.data.api.HkoApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://data.weather.gov.hk/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHkoApiService(retrofit: Retrofit): HkoApiService {
+        return retrofit.create(HkoApiService::class.java)
+    }
+}
