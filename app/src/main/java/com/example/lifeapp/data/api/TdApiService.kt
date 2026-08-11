623b42fd080b923f24c45ba6a916662d47e32689
manandroiddl-app <man.android.dl@gmail.com>
2026-08-11 23:43:36 +0800

    Update TdApiService.kt

--- app/src/main/java/com/example/lifeapp/data/api/TdApiService.kt
+++ app/src/main/java/com/example/lifeapp/data/api/TdApiService.kt
@@ -4,6 +4,6 @@ import com.example.lifeapp.data.model.SpecialTrafficNewsResponse
 import retrofit2.http.GET
 
 interface TdApiService {
-    @GET("td/en/specialtrafficnews.xml")
+    @GET("td/tc/specialtrafficnews.xml")
     suspend fun getSpecialTrafficNews(): SpecialTrafficNewsResponse
 }
