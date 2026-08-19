d17ceb09917bc39b28979251d5e805b8314af77a
manandroiddl-app <man.android.dl@gmail.com>
2026-08-19 23:34:21 +0800

    Update LocationApiService.kt

--- app/src/main/java/com/example/lifeapp/data/api/LocationApiService.kt
+++ app/src/main/java/com/example/lifeapp/data/api/LocationApiService.kt
@@ -6,16 +6,21 @@ import com.example.lifeapp.data.model.GeoJsonFeatureCollection
 import com.example.lifeapp.data.model.KmbStopResponseDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface LocationApiService {
 
-    // 1. CSDI 18 區行政分界 GeoJSON
-    @GET("files/api/dataset/connect-spec/district_boundary.geojson")
-    suspend fun getCsdiDistricts(): Response<GeoJsonFeatureCollection<CsdiDistrictProperties>>
+    // 1. CSDI 18 區行政分界 GeoJSON (支援傳入指定或備援 Url)
+    @GET
+    suspend fun getCsdiDistricts(
+        @Url url: String = "https://api.csdi.gov.hk/files/api/dataset/connect-spec/district_boundary.geojson"
+    ): Response<GeoJsonFeatureCollection<CsdiDistrictProperties>>
 
     // 2. CSDI 街道中線 GeoJSON
-    @GET("files/api/dataset/connect-spec/road_centreline.geojson")
-    suspend fun getCsdiRoads(): Response<GeoJsonFeatureCollection<CsdiRoadProperties>>
+    @GET
+    suspend fun getCsdiRoads(
+        @Url url: String = "https://api.csdi.gov.hk/files/api/dataset/connect-spec/road_centreline.geojson"
+    ): Response<GeoJsonFeatureCollection<CsdiRoadProperties>>
 
     // 3. 九巴全港巴士站清單
     @GET("v1/transport/kmb/stop")
