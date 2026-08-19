530f2370b195b9ab4d0dd3ab75d1989a2c9faa69
manandroiddl-app <man.android.dl@gmail.com>
2026-08-19 12:19:46 +0800

    Update AppDatabase.kt

--- app/src/main/java/com/example/lifeapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/lifeapp/data/local/AppDatabase.kt
@@ -3,10 +3,15 @@ package com.example.lifeapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.lifeapp.data.model.BusBookmarkEntity
+import com.example.lifeapp.data.model.DistrictHierarchyEntity
 import com.example.lifeapp.data.model.LocationEntity
 
 @Database(
-    entities = [BusBookmarkEntity::class, LocationEntity::class],
+    entities = [
+        BusBookmarkEntity::class,
+        LocationEntity::class,
+        DistrictHierarchyEntity::class
+    ],
     version = 2,
     exportSchema = false
 )
