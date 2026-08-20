f1a7c1b91351c4ec1f673dd4c8f205da7e6a16c8
manandroiddl-app <man.android.dl@gmail.com>
2026-08-21 02:11:07 +0800

    Update AppDatabase.kt

--- app/src/main/java/com/example/lifeapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/lifeapp/data/local/AppDatabase.kt
@@ -2,15 +2,14 @@ package com.example.lifeapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.lifeapp.data.model.WeatherCacheEntity
 
 @Database(
     entities = [
-        WeatherCacheEntity::class // 🎯 已移除未定義的 DummyEntity
+        GenericCacheEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun weatherDao(): WeatherDao
+    abstract fun genericCacheDao(): GenericCacheDao
 }
