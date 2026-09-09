c165f0ce1e961601e0bf5b828add97e6f1926658
manandroiddl-app <man.android.dl@gmail.com>
2026-09-09 23:39:10 +0800

    Update AppDatabase.kt

--- app/src/main/java/com/example/lifeapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/lifeapp/data/local/AppDatabase.kt
@@ -3,17 +3,25 @@ package com.example.lifeapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.lifeapp.data.local.dao.TransitBookmarkDao
+import com.example.lifeapp.data.local.dao.TransitDao
 import com.example.lifeapp.data.local.entity.TransitBookmarkEntity
+import com.example.lifeapp.data.local.entity.TransitRouteEntity
+import com.example.lifeapp.data.local.entity.TransitRouteStopEntity
+import com.example.lifeapp.data.local.entity.TransitStopEntity
 
 @Database(
     entities = [
         GenericCacheEntity::class,
-        TransitBookmarkEntity::class
+        TransitBookmarkEntity::class,
+        TransitRouteEntity::class,
+        TransitStopEntity::class,
+        TransitRouteStopEntity::class
     ],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun genericCacheDao(): GenericCacheDao
     abstract fun transitBookmarkDao(): TransitBookmarkDao
+    abstract fun transitDao(): TransitDao
 }
